695613716993d455c688a62c7b8d26f27f478d1c
Danww93 <danww@udel.edu>
2014-10-12 19:58:53 -0400

    CalcEngine

---
+++ CalcEngine/src/CalcFutureValue.java
@@ -0,0 +1,18 @@
+
+public class CalcFutureValue 
+{
+	public static double FutureValue(double amount, int years, double rate)
+	{
+	
+		for(int x = 0; x < (12 * years); x++)
+		{
+			double interest = 0;
+			interest = (rate / 12) * amount;
+			amount = amount + interest;
+			amount = Math.round(amount * 100);
+			amount = amount / 100;
+		}
+		return amount;
+	}
+
+}
